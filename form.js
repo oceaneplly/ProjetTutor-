f7ac5f96a0bdc4d5c635f015660c3bb1a242f952
enwtp <58482806+enwtp@users.noreply.github.com>
2020-01-09 19:40:02 +0100

    Add files via upload

---
+++ form.js
@@ -0,0 +1,6 @@
+function valider(){
+   
+    document.getElementById("formulaire").innerHTML="Merci d'avoir répondu à ce formulaire et d'avoir envoyé une offre de stage à nos étudiants. Votre offre leur sera transmise dans les plus brefs délais."
+    
+}
+
